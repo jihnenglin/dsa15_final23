736b113dc0b9dc2e0e8000bb6344dfdcdd2c712d
Mark <mark11yan@gmail.com>
2015-06-09 12:01:11 +0800

    change output

--- hw6/hw6_2.c
+++ hw6/hw6_2.c
@@ -73,19 +73,19 @@ int main(){
 	for(i = 0; i < 16; i++){
 		char *element = (char*)malloc(sizeof(char) * MAXL);
 		scanf("%s", element);
-		printf("%s\n", element);
+//printf("%s\n", element);
 		void **p = avl_probe(avl_tree, element);
 		void **b = bst_probe(bst_tree, element);
 		void **r = rb_probe(rb_tree, element);
 	}
 	
-	printf("AVL TREE : \n");
+	//printf("AVL TREE : \n");
 	preorder_char_avl(avl_tree->avl_root);
 	printf("\n");
-	printf("BST TREE : \n");
+	//printf("BST TREE : \n");
 	preorder_char_bst(bst_tree->bst_root);
 	printf("\n");
-	printf("RB TREE : \n");
+	//printf("RB TREE : \n");
 	preorder_char_rb(rb_tree->rb_root);
 	printf("\n");
 	
