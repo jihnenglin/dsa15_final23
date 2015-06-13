65191d213b9b364808cf01a5c9dc059f36025279
jihnenglin <b03902005@ntu.edu.tw>
2015-06-13 11:05:47 +0800

    change money to long long int

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -64,19 +64,19 @@ int main(){
 		}else if(strcmp(request, "deposit") == 0){
 			scanf("%lld", &money);
 #ifdef D_INPUT
-			printf("deposit %.0lf\n", money);
+			printf("deposit %lld\n", money);
 #endif
 			// do something here
 		}else if(strcmp(request, "withdraw") == 0){
 			scanf("%lld", &money);
 #ifdef D_INPUT
-			printf("withdraw %.0lf\n", money);
+			printf("withdraw %lld\n", money);
 #endif
 			// do something here
 		}else if(strcmp(request, "transfer") == 0){
 			scanf("%s%lld", id1, &money);
 #ifdef D_INPUT
-			printf("transfer %s %.0lf\n", id1, money);
+			printf("transfer %s %lld\n", id1, money);
 #endif
 			// do something here
 		}else if(strcmp(request, "find") == 0){
